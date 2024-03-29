5a117c8082c91c9f506d2f9a6609cc455a6350ef
yayxs <leaderywl@163.com>
2024-03-29 17:54:24 +0800

    refactor(packages) migrate packages to docs

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'rspress/config'
 
 export default defineConfig({
   // 文档根目录
-  root: 'docs'
+  root: 'docs',
+  themeConfig: {
+    enableScrollToTop: true
+  }
 })
--- wufuhaoyouyi.js
+++
@@ -1,3 +0,0 @@
-const is9 = (9).toString(2)
-
-console.log('>>> run ')
