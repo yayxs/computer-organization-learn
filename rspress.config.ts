30a63ef1873b122d44a97570f19d61091b07ee4b
yayxs <leaderywl@163.com>
2024-04-18 18:53:09 +0800

    feat(mermaid): add mermaid

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'rspress/config'
-
+import { remarkMermaid } from '@theguild/remark-mermaid'
 export default defineConfig({
   // 文档根目录
   root: 'docs',
+  logoText: 'co',
   themeConfig: {
     enableScrollToTop: true
+  },
+  markdown: {
+    mdxRs: false,
+    remarkPlugins: [[remarkMermaid]]
   }
 })
